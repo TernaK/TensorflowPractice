a9d0d0fb0619fe9c4551a8cfd606301263216160
Terna Kpamber <tkpamber@yahoo.com>
2016-02-01 22:26:15 -0800

    Created one NN and one CNN

---
+++ tf01.py
@@ -0,0 +1,35 @@
+"""
+This is a simple neural net with two layers.
+The hidden layer uses 1024 neurons.
+
+It is used for digita classification using the MNIST dataset
+ACCURACY: ~91%
+"""
+import tensorflow as tf
+import numpy as np
+from tensorflow.examples.tutorials.mnist import input_data
+
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+x = tf.placeholder(tf.float32, [None, 784])
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
+y = tf.nn.softmax(tf.matmul(x, W) + b)
+y_ = tf.placeholder(tf.float32, [None, 10])
+cross_entropy = -tf.reduce_sum(y_ * tf.log(y))
+train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
+init = tf.initialize_all_variables()
+sess = tf.Session()
+sess.run(init)
+for i in range(100):
+	batch_xs, batch_ys = mnist.train.next_batch(100)
+	sess.run(train_step, feed_dict={x: batch_xs, y_:batch_ys})
+
+	correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+	accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+	print(sess.run(accuracy, feed_dict={x:mnist.test.images, y_:mnist.test.labels}))
+
+weights = sess.run(W)
+for weight in weights:
+	print(weight)
+sess.close()
